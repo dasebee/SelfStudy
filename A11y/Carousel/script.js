451a28e520c77b1ef5588f36ece8c3021248afaa
Dasom Kim <babam1012@gmail.com>
2017-07-17 12:13:39 +0900

    키보드와 인디케이터로컨트롤하기

--- A11y/Carousel/script.js
+++ A11y/Carousel/script.js
@@ -1,12 +1,9 @@
 (function(global, document){
  'use strict'
- //해야하는것.
- //애니메이션
- //키보드 버튼 이벤트 <- ->
- //인디케이터를 눌렀을때의 움직임...(class를 따로 줘서 float :left로 한다음에 ul을 움직이게 하는 거... 가능?)
+
  
  //변수들 
-  var next_btn, prev_btn, indicators, panels, panel_box, img_width, active_img, next_img, prev_img, active_tab;
+  var next_btn, prev_btn, indicators, panels, panel_box, img_width, active_img, next_img, prev_img, active_tab, float_img;
   var forEach = Array.prototype.forEach;
  
  
@@ -22,6 +19,7 @@
   active_img = panel_box.querySelector('.active-img');
   next_img = panel_box.querySelector('.next-img');
   prev_img = panel_box.querySelector('.prev-img');
+  float_img = document.querySelector('.float-img');
   // active tab 찾기
   forEach.call(indicators, function(el){
     if(el.classList.contains('active-tab')){
@@ -34,14 +32,17 @@
     indicators[i].classList.add('active-tab');
  }
  function viewChangeByTab(i){
-  var direction = i - active_img.index;
-  if(direction > 0){
-    showNext(i);
+   panel_box.classList.add('float-img');
+   var current_position = active_img.index * img_width;
+   var target_position = ((i * img_width) - current_position) *-1;
+   panel_box.style.transform='translateX(-'+ current_position+ 'px)';
+   forEach.call(panels, function(el){
+    el.style.transform='translateX('+ target_position+ 'px)';
+    el.style.transition='all 1s ease-in';
+   })
+   changeTab(i);
+   setTimeout(changeClass.bind(undefined,i), 1100);
 
-  }
-  if(direction < 0){
-    showPrev(i);
-  }
  }
  function showNext(i){
    findClass();
@@ -49,7 +50,7 @@
    if(i===5) { i = 0; }
    changeTab(i);
    panels[i].classList.add('next-img');
-   setTimeout(changeClass.bind(undefined,i), 1000);
+   setTimeout(changeClass.bind(undefined,i), 1100);
  }
  function showPrev(i){
    findClass();
@@ -57,7 +58,7 @@
     if(i===-1) { i = 4; }
     changeTab(i);
     panels[i].classList.add('next-img');
-    setTimeout(changeClass.bind(undefined,i), 1000);
+    setTimeout(changeClass.bind(undefined,i), 1100);
     panels[i].classList.add('prev-img');
  }
  function changeClass(i){
@@ -66,38 +67,46 @@
     panels[i].classList.add('active-img');
     !next_img || next_img.classList.remove('next-img');
     !prev_img || prev_img.classList.remove('prev-img');
+    if(float_img){
+      float_img.classList.remove('float-img');
+      panel_box.removeAttribute('style');
+      forEach.call(panels, function(el){
+        el.removeAttribute('style');
+      })
+    }
  }
 
  
  Carousel.fn = Carousel.prototype;
  Carousel.fn.init = function(){
-  if( !this) { throw 'new를 사용해주세요.'}
-  img_width = 1500;
-  next_btn = document.getElementsByClassName('next-button')[0];
-  prev_btn = document.getElementsByClassName('prev-button')[0];
-  indicators = document.getElementsByClassName('indicator');
-  panels = document.getElementsByClassName('slide-img');
-  panel_box = document.getElementsByClassName('panel-box')[0];
+    if( !this) { throw 'new를 사용해주세요.'}
+    img_width = 1500;
+    next_btn = document.getElementsByClassName('next-button')[0];
+    prev_btn = document.getElementsByClassName('prev-button')[0];
+    indicators = document.getElementsByClassName('indicator');
+    panels = document.getElementsByClassName('slide-img');
+    panel_box = document.getElementsByClassName('panel-box')[0];
 
 
-  //탭에 이벤트 추가
-  next_btn.addEventListener('click',Carousel.fn.nextSlide);
-  prev_btn.addEventListener('click',Carousel.fn.prevSlide);
-  forEach.call(indicators, function(el,i){
-    el.onclick=Carousel.fn.changeIndicator.bind(el,i);
-  })
-  //이미지들에 인덱스 추가
-  forEach.call(panels, function(el,i){
-    el.index = i;
-  })
+    //탭에 이벤트 추가
+    next_btn.addEventListener('click',Carousel.fn.nextSlide);
+    next_btn.onkeydown = Carousel.fn.KeyboardNavigation.bind(undefined,undefined);
+    prev_btn.addEventListener('click',Carousel.fn.prevSlide);
+    prev_btn.onkeydown = Carousel.fn.KeyboardNavigation.bind(undefined,undefined);
+    forEach.call(indicators, function(el,i){
+      el.onclick=Carousel.fn.changeIndicator.bind(el,i);
+      el.onkeydown = Carousel.fn.KeyboardNavigation.bind(el,i);
+    })
+    //이미지들에 인덱스 추가
+    forEach.call(panels, function(el,i){
+      el.index = i;
+    })
+
  }
 
  Carousel.fn.palyAnimation = function(){
-
  }
- Carousel.fn.stopAnimation = function(){
 
- }
  Carousel.fn.nextSlide = function(){
   showNext();
  }
@@ -110,10 +119,17 @@
   findClass();
   viewChangeByTab(i);
 }
- Carousel.fn.nextSlideByKeyboard = function(){
- }
- Carousel.fn.prevSlideByKeyboard = function(){
- }
+ Carousel.fn.KeyboardNavigation = function(i, e){
+   var keyCode = e.keyCode;
+   if(keyCode === 37 || keyCode === 38){
+     showPrev();
+  } else if(keyCode === 39 || keyCode === 40){
+     showNext();
+   } else{
+     return;
+   }
+}
+
 global.Carousel = Carousel;
 })(window, window.document);
 
