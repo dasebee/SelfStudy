5fb11f2c8b01c45182a41a97a8152a08bc2e4782
Dasom Kim <babam1012@gmail.com>
2017-07-23 20:57:41 +0900

    Firebase에서 데이터를 가져와서 화면에 표시, Firebase에 데이터 추가

--- Vue/TodoList/dist/todo.js
+++ Vue/TodoList/dist/todo.js
@@ -11,34 +11,61 @@
     messagingSenderId: "137140748409"
   };
   firebase.initializeApp(config);
-  let firebase_db = firebase.database();
+//   let data ={             
+//     task: [], 
+//     complete : []
+//   }         
 
-  function addTodo(){
-    firebase.database().ref('todolist/').set({
-             task: [{
-               content:'test!!',
-               complete: false
-             }], 
-             complete : [{
-               content:'test??',
-               complete: true
-             }]         
-              })
-  };
+//   function addTodo(){
+//     firebase.database().ref('todolist/').set(
+//       data);}
   
-  addTodo();
+//   addTodo();
   
-  // let app = new Vue({
-  //   el: '#app',
-  //   data:{
-  //     todo : {
-  //       active:[],
-  //       completed :[]
-  //     }
-  //  },
-  //  computed:{},
-  //  methods: {} 
-  // })
+// firebase.database().ref('todolist/').once('value', function(res){
+//   res.forEach(function(todo){
+//     console.log(todo);
+//   })
+// })
+  
+  let app = new Vue({
+    el: '#app',
+    created(){
+      this.getToDoData();
+    },
+    data:{
+      todos : [],
+      input_value : ''
+    },
+    computed:{},
+    methods: {
+      getToDoData(){
+        //todos 배열 초기화
+        this.todos = [];
+        // //db의 data를 가져와서 배열에 push
+        firebase.database().ref().once('value', (snapshot)=>{
+          snapshot.forEach((todo)=>{
+            //db에서 가져온 데이터에 key값을 합치기.
+            this.todos.push(Object.assign(todo.val(),{key: todo.key}))
+          })
+        });
+      },
+      inputValue(e){
+        this.input_value = e.target.value.trim();
+      },
+      addTodoData(){
+        if(this.input_value === ''){ throw '값을 입력해주세요.' }
+        firebase.database().ref().push({
+          content: this.input_value,
+          completed: false
+        })
+        document.querySelector('.todoinput').value ='';
+        this.input_value = '';
+        this.getToDoData();
+      },
+    } 
+  })
+  
 
   
 })(window, window.Vue);
