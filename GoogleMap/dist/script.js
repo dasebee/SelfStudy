8eb481d0dc46d7dca8cb8c9531f8f295238dcf2f
Dasom Kim <babam1012@gmail.com>
2017-07-24 23:35:39 +0900

    숙소에 마우스 올리면 마커 색 변경

--- GoogleMap/dist/script.js
+++ GoogleMap/dist/script.js
@@ -3,7 +3,7 @@
  * 초기 설정시의 zoom 값, 위치 범위가 넓으면 zoom을 작게 하도록. 
  * 방에 포커스, 호버가 가면 지도의 마커의 색이 바뀐다. 
  * infowindow의 스타일!!!!
- * infowindow가 표시될 때. 마커가 사라지고.
+ * infowindow가 표시될 때. 마커가 사라지고. 레이블도 사라지고. 
  * 표시된 info를 누르면 해당 방의 상세 페이지로 이동. 
  * 상세 페이지의 지도는 대략적인 범위만 알려주는 동그라미만 표시함. 
  * 
@@ -11,38 +11,44 @@
  * 
  **/
 
- 
 
 
 (function(global, document, google){
  'use strict'
   var map, markers, center_lat = 0, center_lng = 0, location = {lat: -33.91722, lng: 151.23064};
   var map_el = document.getElementById('map'); //마운트할 위치 찾기
-  var room = document.getElementsByClassName('room')[0];
+  var rooms = document.querySelectorAll('li');
   var icon_span= '<span> 111</span>'
   var info_window_content= document.getElementsByClassName('room')[0].innerHTML;  //innerHTML 안하면 요소자체가 infowindow로 이동해 버린다!
-   var locations =[    
-     {lat: -33.91721, lng: 151.22630, price: '293,844' },
-     {lat: -33.91539, lng: 151.22820, price: '99,327'},
-     {lat: -33.91747, lng: 151.22912, price: '317,4847'},
-     {lat: -33.91910, lng: 151.22907, price: '152,873'},
-     {lat: -33.91725, lng: 151.23011, price: '573,217'},
-     {lat: -33.91872, lng: 151.23089, price: '93,474'},
-     {lat: -33.91784, lng: 151.23094, price: '94,797'},
-     {lat: -33.91682, lng: 151.23149, price: '328,971'},
-     {lat: -33.91790, lng: 151.23463, price: '237,124'},
-     {lat: -33.91666, lng: 151.23468, price: '487,457'},
-     {lat: -33.916988, lng: 151.233640, price: '214,735'},
-     {lat: -33.91662347903106, lng: 151.22879464019775, price: '387,574'},
-     {lat: -33.916365282092855,lng:  151.22937399734496, price: '147,216'},
-     {lat: -33.91665018901448, lng: 151.2282474695587, price: '138,754'},
-     {lat: -33.919543720969806,lng:  151.23112279762267, price: '104,857'},
-     {lat: -33.91608037421864, lng: 151.23288232673644, price: '931,874'},
-     {lat: -33.91851096391805, lng: 151.2344058214569, price: '587,122'},
-     {lat: -33.91818154739766, lng: 151.2346203981781, price: '957,265'},
-     {lat: -33.91727341958453, lng: 151.23348314155578, price: '479,814'}];
+  var locations =[    
+    {lat: -33.91721, lng: 151.22630, price: '293,844' },
+    {lat: -33.91539, lng: 151.22820, price: '99,327'},
+    {lat: -33.91747, lng: 151.22912, price: '317,4847'},
+    {lat: -33.91910, lng: 151.22907, price: '152,873'},
+    {lat: -33.91725, lng: 151.23011, price: '573,217'},
+    {lat: -33.91872, lng: 151.23089, price: '93,474'},
+    {lat: -33.91784, lng: 151.23094, price: '94,797'},
+    {lat: -33.91682, lng: 151.23149, price: '328,971'},
+    {lat: -33.91790, lng: 151.23463, price: '237,124'},
+    {lat: -33.91666, lng: 151.23468, price: '487,457'},
+    {lat: -33.916988, lng: 151.233640, price: '214,735'},
+    {lat: -33.91662347903106, lng: 151.22879464019775, price: '387,574'},
+    {lat: -33.916365282092855,lng:  151.22937399734496, price: '147,216'},
+    {lat: -33.91665018901448, lng: 151.2282474695587, price: '138,754'},
+    {lat: -33.919543720969806,lng:  151.23112279762267, price: '104,857'},
+    {lat: -33.91608037421864, lng: 151.23288232673644, price: '931,874'},
+    {lat: -33.91851096391805, lng: 151.2344058214569, price: '587,122'},
+    {lat: -33.91818154739766, lng: 151.2346203981781, price: '957,265'},
+    {lat: -33.91727341958453, lng: 151.23348314155578, price: '479,814'}];
   
-
+var map_icon = {
+  path: fontawesome.markers.COMMENT,
+  scale: 0.8,
+  strokeWeight: 1,
+  strokeColor: 'black',
+  strokeOpacity: 1,
+  fillColor: '#f8ae5f',
+  fillOpacity: 1}      
 //locations의 위치 데이터 값의 평균을 구해서 맵의 초기 센터위치로 지정.      
 locations.forEach(function(location){
   center_lat+= location.lat;
@@ -70,31 +76,39 @@ initMap();
  var markers = locations.map(function(location) {
     return new MarkerWithLabel({
       position: new google.maps.LatLng(location.lat, location.lng),
-      draggable: true,
+      draggable: false,
       raiseOnDrag: true,
       map: map,
       labelContent: location.price,
       labelAnchor: new google.maps.Point(-5, 30),
       labelClass: "map-labels", // the CSS class for the label
       labelStyle: {opacity: 0.75},
-      icon:{
-        path: fontawesome.markers.COMMENT,
-        scale: 0.8,
-        strokeWeight: 1,
-        strokeColor: 'black',
-        strokeOpacity: 1,
-        fillColor: '#f8ae5f',
-        fillOpacity: 1,
-      }
+      icon: map_icon
     })
   });
 
 
+
 //각각의 마커에 이벤트 추가.         
 markers.forEach(function(marker){
-    marker.addListener('click', function() {
-         infowindow.open(map, marker); //infoWindow을 map의 marker 위치에서 연다. 
-        });
+  marker.addListener('click', function() {
+    infowindow.open(map, marker); //infoWindow을 map의 marker 위치에서 연다. 
+  });
+})
+
+// markers[0].icon.fillColor="#ff3333"
+
+
+// // 숙소 리스트에 접근하였을 때, 마커의 색변경
+rooms.forEach(function(room, index){
+  room.addEventListener('mouseover', function(){
+    map_icon.fillColor = '#ff3333';
+    markers[index].setIcon(map_icon);
+  })
+  room.addEventListener('mouseleave', function(){
+    map_icon.fillColor = '#f8ae5f';
+    markers[index].setIcon(map_icon);
+  })
 })
 
 
